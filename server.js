11a3dc711da68d2a230fa9e48c3e356eb8569bc3
Namles256 <160243662+Namles256@users.noreply.github.com>
2025-05-27 00:02:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
@@ -15,7 +14,7 @@ const io = new Server(server, {
 const rooms = {};
 
 app.get("/", (req, res) => {
-  res.send("✅ Buzzer-Backend läuft.");
+  res.send("✅ Buzzer-Backend läuft (v0.1)");
 });
 
 io.on("connection", (socket) => {
@@ -26,109 +25,28 @@ io.on("connection", (socket) => {
     if (!rooms[room]) {
       rooms[room] = {
         players: {},
-        host: null,
-        showPoints: true,
-        allowMultipleBuzzers: false,
-        hideBuzzFromPlayers: false,
-        onlyFirstBuzz: false,
-        buzzLocked: false
+        host: null
       };
     }
 
     if (isHost) {
       rooms[room].host = socket.id;
     } else {
-      if (!rooms[room].players[name]) {
-        rooms[room].players[name] = 0;
-      }
+      rooms[room].players[name] = true;
     }
 
-    updatePlayers(room);
-  });
-
-  socket.on("settings", ({ room, showPoints, allowMultipleBuzzers, hideBuzzFromPlayers, onlyFirstBuzz }) => {
-    if (rooms[room]) {
-      if (showPoints !== undefined) rooms[room].showPoints = showPoints;
-      if (allowMultipleBuzzers !== undefined) rooms[room].allowMultipleBuzzers = allowMultipleBuzzers;
-      if (hideBuzzFromPlayers !== undefined) rooms[room].hideBuzzFromPlayers = hideBuzzFromPlayers;
-      if (onlyFirstBuzz !== undefined) rooms[room].onlyFirstBuzz = onlyFirstBuzz;
-      updatePlayers(room);
-    }
+    io.to(rooms[room].host).emit("players", Object.keys(rooms[room].players));
   });
 
   socket.on("buzz", ({ room, name }) => {
-    if (!rooms[room]) return;
-    const roomData = rooms[room];
-    if (roomData.onlyFirstBuzz && roomData.buzzLocked) return;
-    roomData.buzzLocked = roomData.onlyFirstBuzz;
-    io.to(room).emit("buzz", { name });
-  });
-
-  socket.on("result", ({ room }) => {
     if (rooms[room]) {
-      rooms[room].buzzLocked = false;
-      io.to(room).emit("reset");
+      io.to(rooms[room].host).emit("buzz", { name });
     }
   });
 
   socket.on("resetBuzz", (room) => {
-    if (rooms[room]) {
-      rooms[room].buzzLocked = false;
-      io.to(room).emit("reset");
-    }
+    io.to(room).emit("reset");
   });
-
-  socket.on("resetPoints", (room) => {
-    if (rooms[room]) {
-      for (const name in rooms[room].players) {
-        rooms[room].players[name] = 0;
-      }
-      updatePlayers(room);
-    }
-  });
-
-  socket.on("setPoints", ({ room, playerName, points }) => {
-    if (rooms[room] && rooms[room].players[playerName] !== undefined) {
-      rooms[room].players[playerName] = points;
-      updatePlayers(room);
-    }
-  });
-
-  socket.on("resetRoom", (room) => {
-    if (rooms[room]) {
-      rooms[room].players = {};
-      updatePlayers(room);
-    }
-  });
-
-  socket.on("disconnect", () => {
-    const { room, name } = socket.data || {};
-    if (rooms[room] && rooms[room].players[name]) {
-      delete rooms[room].players[name];
-      updatePlayers(room);
-    }
-  });
-
-  function updatePlayers(room) {
-    const roomData = rooms[room];
-    const players = roomData.players;
-
-    // Host bekommt immer alles
-    if (roomData.host) {
-      io.to(roomData.host).emit("players", players);
-    }
-
-    for (const [id, socket] of io.of("/").sockets) {
-      if (socket.data.room === room && !socket.data.isHost) {
-        if (roomData.showPoints) {
-          io.to(id).emit("players", players);
-        } else {
-          const empty = Object.fromEntries(Object.keys(players).map((p) => [p, 0]));
-          io.to(id).emit("players", empty);
-        }
-      }
-    }
-  }
 });
 
 server.listen(3001, () => {
