16de5132fd0497281bc21cdf9c7d7c6d652fcf8d
Namles256 <160243662+Namles256@users.noreply.github.com>
2025-05-30 14:23:15 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const io = new Server(server, {
 const rooms = {};
 
 app.get("/", (req, res) => {
-  res.send("✅ Buzzer-Backend läuft (v0.4.4.2)");
+  res.send("✅ Buzzer-Backend läuft (v0.4.4.3)");
 });
 
 io.on("connection", (socket) => {
