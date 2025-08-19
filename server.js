055f4fe746211d1551e75f0ca7959efba3df8dd9
Namles256 <160243662+Namles256@users.noreply.github.com>
2025-08-19 14:19:01 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -125,21 +125,7 @@ socket.on("stopTimer", (room) => {
     socket.emit("mcAnswers", rooms[room].mcAnswers || {});
   });
 	socket.on("hostBuzzLockChanged", ({ room, locked }) => {
-  io.to(room).emit("hostBuzzLockChanged", { locked });
-
-  if (!rooms[room]) return;
-
-  // Wenn ENTSPERRT wird...
-  if (!locked) {
-    const allNames = Object.keys(rooms[room].players || {});
-    const allSurrendered = allNames.length > 0 && allNames.every(n => rooms[room].surrender?.[n]);
-
-    if (allSurrendered) {
-      // Nur wenn ALLE surrendern → auch die Fahnen zurücksetzen
-      Object.keys(rooms[room].surrender).forEach(p => rooms[room].surrender[p] = false);
-      io.to(room).emit("surrenderUpdate", rooms[room].surrender);
-    }
-	}
+	io.to(room).emit("hostBuzzLockChanged", { locked });
 	});
   socket.on("settings", (data) => {
     const room = socket.data.room;
@@ -199,40 +185,26 @@ socket.on("stopTimer", (room) => {
     }
   });
 
-	socket.on("buzz", ({ room, name }) => {
-	  if (!rooms[room]) return;
-
-	  // Spezialfall: Timer-Ende → simulierter Buzz
-	  if (name === "_timer_end_") {
-		// Nur wenn Buzzer noch nicht blockiert ist
-		if (!rooms[room].buzzedName && !rooms[room].buzzLocked) {
-		  rooms[room].buzzedName = name;
-		  rooms[room].buzzLocked = true;
-		  io.to(room).emit("buzzBlocked");
-		}
-		return;
-	  }
-
-	  if (!rooms[room].players[name]) return;
-
-	  if (rooms[room].buzzLocked) return;
-
-	  if (rooms[room].buzzMode === "first") {
-		if (!rooms[room].buzzedName) {
-		  rooms[room].buzzedName = name;
-		  rooms[room].buzzLocked = true;
-		  io.to(room).emit("buzz", { name });
-		  io.to(room).emit("buzzBlocked");
-		  io.to(room).emit("playBuzzSound");
-		}
-	  } else if (rooms[room].buzzMode === "multi") {
-		if (!rooms[room].multiBuzzed.includes(name)) {
-		  rooms[room].multiBuzzed.push(name);
-		  io.to(room).emit("buzz", { name });
-		  io.to(room).emit("playBuzzSound");
-		}
-	  }
-	});
+  socket.on("buzz", ({ room, name }) => {
+    if (!rooms[room]) return;
+    const buzzMode = rooms[room].settings.buzzMode || "first";
+	if (rooms[room].settings.resetOnBuzz && rooms[room].timerRunning) {
+  rooms[room].timerRunning = false;
+  io.to(room).emit("stopTimer");
+}
+    if (buzzMode === "first") {
+      if (rooms[room].buzzed) return;
+      rooms[room].buzzed = name;
+      rooms[room].buzzOrder = [name];
+      io.to(room).emit("playBuzzSound");
+    } else if (buzzMode === "multi") {
+      if (!rooms[room].buzzOrder.includes(name)) {
+        rooms[room].buzzOrder.push(name);
+      }
+    }
+    io.to(room).emit("buzz", { name });
+    emitPlayerUpdate(room);
+  });
 
   socket.on("result", ({ room, name, type }) => {
     if (!rooms[room]) return;
